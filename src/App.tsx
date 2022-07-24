c63b7240e76463704d34cdc9014e8c34f45f2fc2
Rumata-SL <kirr7619@gmail.com>
2022-07-24 19:34:16 +0500

    fixed bug request

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import {Cards} from "./component/cards/Cards";
 import {Header} from "./component/header/Header";
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Card} from "./component/card/Card";
 
 function App() {
-  return (
-    <div className="App">
-
-          <div className={"wrapper"}>
-          <Header/>
-              <div className={"container"}>
-                  <Routes>
-                  <Route path={"/"} element={<Navigate to={"/cards/"}/>}/>
-                  <Route path={"/cards/"} element={<Cards/>}></Route>
-                  <Route path={"/card/"} element={<Card/>}></Route>
-              </Routes>
-              </div>
-          </div>
-
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={"wrapper"}>
+                <Header/>
+                <div className={"container"}>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={"/cards/"}/>}/>
+                        <Route path={"/cards/"} element={<Cards/>}></Route>
+                        <Route path={"/card/:id"} element={<Card/>}></Route>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
